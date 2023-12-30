e804863fe3605316a8f9be0c80f96e8543b60922
khan2626 <israelkhan217@gmail.com>
2023-12-30 07:08:02 +0100

    Task 0-7

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -7,8 +7,10 @@ listening in port 0.0.0.0
 /c/<text>: display “C ” followed by the value of the text variable
 /python/<text>: display “Python ”, followed by the value of the text
 /number/<n>: display “n is a number” only if n is an integer
+/number_template/<n>: display a HTML page only if n is an integer
 """
-from flask import Flask
+from flask import Flask, render_template
+
 
 app = Flask(__name__)
 
@@ -43,6 +45,13 @@ def number(n):
     """ Returns "n is a number" if n is an integer """
     if isinstance(n, int):
         return (f'{n} is a number')
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_template(n):
+    """Returns a string"""
+    if isinstance(n, int):
+        return render_template('5-number.html', n=n)
 
 
 if __name__ == "__main__":
